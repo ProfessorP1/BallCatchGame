f6a4520e4b3cd84e3fe45866500b86c2d2a0a8ce
ProfessorP1 <cooleeisgreme.baum@gmail.com>
2024-07-21 23:32:37 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('wss://your-websocket-server-url'); // Ersetze dies durch die URL deines WebSocket-Servers
+const socket = new WebSocket('wss://https://scoreboard4.glitch.me'); // Ersetze dies durch die URL deines WebSocket-Servers
 
 document.getElementById('left').addEventListener('click', () => {
   sendCommand('left');
